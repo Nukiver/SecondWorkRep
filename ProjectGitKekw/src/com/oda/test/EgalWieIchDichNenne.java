205200840da0a202e514f877165fd30afa2a13a9
Max, Leber <cryptomane88@gmail.com>
2021-02-16 15:00:12 +0100

    Neue Zeile

--- ProjectGitKekw/src/com/oda/test/EgalWieIchDichNenne.java
+++ ProjectGitKekw/src/com/oda/test/EgalWieIchDichNenne.java
@@ -6,6 +6,7 @@ public class EgalWieIchDichNenne {
 		
 		System.out.println("Ich bin egal.");
 		System.out.println("Es ist egal wie ich heiﬂe.");
+		System.out.println("any PogChamp in chat? no? Sadge");
 		
 	}
 	
